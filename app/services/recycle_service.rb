f1b03b194c0ee25b43970ce1e8cea9e3f784c6b8
Michael Mattson <michael.p.mattson@gmail.com>
2021-12-02 19:50:09 -0700

    Add get_materials

--- app/services/recycle_service.rb
+++ app/services/recycle_service.rb
@@ -9,5 +9,9 @@ class RecycleService
     def search_locations(params)
       ApiClient.get_data("#{BASE_URL}searchLocations?api_key=#{ENV['EARTH911_KEY']}&material_id=#{params[:material_id]}&longitude=#{params[:longitude]}&latitude=#{params[:latitude]}")
     end
+
+    def get_materials
+      ApiClient.get_data("#{BASE_URL}getMaterials?api_key=#{ENV['EARTH911_KEY']}")
+    end
   end
 end
--- spec/services/recycle_service_spec.rb
+++ spec/services/recycle_service_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe RecycleService, :vcr do
         longitude: -112.04
       }
 
-      results     = RecycleService.search_locations(params)
+      results = RecycleService.search_locations(params)
 
       expect(results).to be_a(Hash)
 
@@ -38,31 +38,21 @@ RSpec.describe RecycleService, :vcr do
         expect(r[:longitude]).to be_a(Float)
       end
     end
+
+    it 'returns list of all materials' do
+      results = RecycleService.get_materials
+
+      expect(results).to be_a(Hash)
+
+      expect(results[:num_results]).to be_an(Integer)
+      expect(results[:result]).to be_an(Array)
+
+      expect(results[:result].first[:description]).to be_a(String)
+      expect(results[:result].first[:material_id]).to be_an(Integer)
+      expect(results[:result].first[:long_description]).to be_a(String)
+      expect(results[:result].first[:image]).to be_a(String)
+      expect(results[:result].first[:family_ids]).to be_an(Array)
+      expect(results[:result].first[:family_ids]&.first).to be_an(Integer)
+    end
   end
 end
-
-
-# {
-#     "search_time": "00.653081",
-#     "num_results": 12,
-#     "result": [
-#         {
-#             "curbside": false,
-#             "description": "eGreen IT Solutions",
-#             "distance": 7.0,
-#             "longitude": -112.11036529435061,
-#             "latitude": 33.587015133686904,
-#             "location_type_id": 0,
-#             "location_id": "Q1RQNVdbXV9AVQ",
-#             "municipal": false
-#         },
-#         {
-#             "curbside": false,
-#             "description": "Maricopa County Transfer Station - Cave Creek",
-#             "distance": 8.0,
-#             "longitude": -112.00889446883295,
-#             "latitude": 33.78308129118068,
-#             "location_type_id": 1,
-#             "location_id": "Q1RQNVBfW1xAUg",
-#             "municipal": true
-#         },
