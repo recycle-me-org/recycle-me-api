76f99bd6ccf5cc77c0c497a68aeb46d4738ff01a
Henry Schmid <henry.schmid1@gmail.com>
2021-12-04 12:32:48 -0700

    Add schema test

---
+++ spec/graphql/recycle_me_api_schema.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe RecycleMeApiSchema do
+  it 'matches the dumped schema (rails graphql:schema:dump)' do
+    aggregate_failures do
+      expect(described_class.to_definition).to eq(File.read(Rails.root.join('schema.graphql')))
+      expect(described_class.to_json).to eq(File.read(Rails.root.join('schema.json')).rstrip)
+    end
+  end
+end
