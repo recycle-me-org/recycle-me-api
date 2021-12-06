53dfb5ea7601cf3b17257b10319c14acf716d566
Henry Schmid <henry.schmid1@gmail.com>
2021-12-06 11:04:34 -0700

    Change origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'https://recycle-me-api.herokuapp.com/'
 
     resource '*',
       headers: :any,
