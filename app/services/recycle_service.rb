73cdb9de5104d7e77eb9f642c88909f69ac3ebda
Henry Schmid <henry.schmid1@gmail.com>
2021-12-07 15:44:22 -0700

    Add method for getting location details

--- app/services/recycle_service.rb
+++ app/services/recycle_service.rb
@@ -1,17 +1,23 @@
 class RecycleService
   BASE_URL = 'http://api.earth911.com/earth911.'
+  KEY = "api_key=#{ENV['EARTH911_KEY']}"
 
   class << self
     def search_materials(material)
-      ApiClient.get_data("#{BASE_URL}searchMaterials?api_key=#{ENV['EARTH911_KEY']}&query=#{material}")
-    end
-
-    def search_locations(material_id, lat, long)
-      ApiClient.get_data("#{BASE_URL}searchLocations?api_key=#{ENV['EARTH911_KEY']}&material_id=#{material_id}&longitude=#{long}&latitude=#{lat}")
+      ApiClient.get_data("#{BASE_URL}searchMaterials?#{KEY}&query=#{material}")
     end
 
     def get_materials
-      ApiClient.get_data("#{BASE_URL}getMaterials?api_key=#{ENV['EARTH911_KEY']}")
+      ApiClient.get_data("#{BASE_URL}getMaterials?#{KEY}")
+    end
+
+    def search_locations(material_id, lat, long)
+      ApiClient.get_data("#{BASE_URL}searchLocations?#{KEY}&material_id=#{material_id}&longitude=#{long}&latitude=#{lat}")
+    end
+
+    def get_location_details(location_ids)
+      ApiClient.get_data("#{BASE_URL}getLocationDetails?#{KEY}&location_id=#{location_ids}")
     end
   end
 end
+  
