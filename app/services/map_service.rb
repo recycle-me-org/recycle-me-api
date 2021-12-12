e2191e1ced06d87eb254884e062032743edcf2b7
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-12-11 18:23:25 -0700

    Update get place info method to use a different end point

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,8 +2,8 @@ class MapService
   BASE_URL = 'https://maps.googleapis.com/maps/api/'
   KEY = "key=#{ENV['GOOGLE_MAP_KEY']}"
   class << self
-    def get_place_info(address)
-      ApiClient.get_data("#{BASE_URL}geocode/json?address=#{address}&#{KEY}")
+    def get_place_info(lat, long, keyword)
+      ApiClient.get_data("#{BASE_URL}place/nearbysearch/json?location=#{lat}%2C#{long}&radius=20&keyword=#{keyword}&#{KEY}")
     end
   end
 end
