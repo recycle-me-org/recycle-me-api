03d199b7ecf377d80834b807134ed575c4a7612e
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-12-11 18:53:05 -0700

    Update zip code

--- spec/graphql/queries/search_location_spec.rb
+++ spec/graphql/queries/search_location_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'searching locations', :vcr, type: :request do
   describe 'search' do
     it 'returns locations for given search query' do
-      post '/graphql', params: { query: query(materialId: 104, location: '80031, United States') }
+      post '/graphql', params: { query: query(materialId: 104, location: '80217, United States') }
 
       json = JSON.parse(response.body, symbolize_names: true)
       locations = json[:data][:searchLocations]
